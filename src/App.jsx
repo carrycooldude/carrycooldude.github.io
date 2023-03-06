5507bdde1a29f1223f21ed6eec2e459ea9c95d42
carrycooldude <rawatkari554@gmail.com>
2023-03-07 00:16:13 +0530

    Working on App.jsx for starting the class-naming

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter } from "react-router-dom";
+import { About , Contact , Experience , Navbar , Hero , Tech , Works , Feedbacks , StarsCanvas } from "./components";
+const App = () => {
+
+  return (
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover
+      bg-no-repeat bg-center">
+        < Navbar />
+        < Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <StarsCanvas />
+        <Contact />
+      </div>
+
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App

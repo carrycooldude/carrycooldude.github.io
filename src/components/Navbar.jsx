046d19b04568568c5b8023ae8b713af0f5e83a9f
carrycooldude <rawatkari554@gmail.com>
2023-03-07 00:32:27 +0530

    Changed the name of style.js to styles.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React , {useEffect , useState} from 'react';
+import { Link } from "react-router-dom";
+import {styles} from "../styles";
+import { navLinks } from '../constants';
+import { logo , menu , close } from '../assets';
 
 const Navbar = () => {
   return (

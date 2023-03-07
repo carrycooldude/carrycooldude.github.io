fdd5b1b7177729dd990c6d580a6427ffa9076236
carrycooldude <rawatkari554@gmail.com>
2023-03-07 12:39:30 +0530

    NavBar Intitialers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,25 @@ import { navLinks } from '../constants';
 import { logo , menu , close } from '../assets';
 
 const Navbar = () => {
+  const [active , setActive] = useState('`');  
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddindX} w-full flex items-center 
+    py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link 
+        to='/' 
+        className="flex items-center gap-2"
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0,0);
+        }}>
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <p className="text-white text-[18px] font-bold cursor-pointer">Kartikey <span className="sm:block hidden">Rawat</span></p>
+        </Link>
+
+      </div>
+
+    </nav>
   )
 }
 

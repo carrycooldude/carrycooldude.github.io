95e4f8f937b07082aed0083a430a2ec75e11c2de
carrycooldude <rawatkari554@gmail.com>
2023-03-07 22:36:54 +0530

    Chaned Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,21 +5,36 @@ import { navLinks } from '../constants';
 import { logo , menu , close } from '../assets';
 
 const Navbar = () => {
-  const [active , setActive] = useState('`');  
+  const [active , setActive] = useState('');  
   return (
     <nav className={`${styles.paddindX} w-full flex items-center 
     py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
-        to='/' 
+        to="/" 
         className="flex items-center gap-2"
         onClick={() => {
           setActive("");
           window.scrollTo(0,0);
         }}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
           <p className="text-white text-[18px] font-bold cursor-pointer">Kartikey <span className="sm:block hidden">Rawat</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row ggap-10">
+          {navLinks.map((link)=>{
+            <li  
+            key={link.id}
+            className={`${
+              active === link.title
+              ? "text-white"
+              : "text-secondary"
+          } hover:text-white text-[18px] font-medium cursor-pointer`}
+          >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          })}
+
+        </ul>
 
       </div>
 

b409d40ac73c2446f5c480ccbd7e15ab14b55624
carrycooldude <rawatkari554@gmail.com>
2023-03-08 12:21:50 +0530

    Resolve the NavBar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,32 +7,36 @@ import { logo , menu , close } from '../assets';
 const Navbar = () => {
   const [active , setActive] = useState('');  
   return (
-    <nav className={`${styles.paddindX} w-full flex items-center 
-    py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={
+      `${styles.paddingX} w-full flex items-center 
+    py-5 fixed top-0 z-20 bg-primary`}
+    >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
-        to="/" 
-        className="flex items-center gap-2"
-        onClick={() => {
-          setActive("");
-          window.scrollTo(0,0);
-        }}>
+          to="/" 
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0,0);
+          }
+        }>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-          <p className="text-white text-[18px] font-bold cursor-pointer">Kartikey <span className="sm:block hidden">Rawat</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Kartikey <span className="sm:block hidden">Rawat</span></p>
         </Link>
-        <ul className="list-none hidden sm:flex flex-row ggap-10">
-          {navLinks.map((link)=>{
-            <li  
-            key={link.id}
-            className={`${
-              active === link.title
-              ? "text-white"
-              : "text-secondary"
-          } hover:text-white text-[18px] font-medium cursor-pointer`}
-          >
-              <a href={`#${link.id}`}>{link.title}</a>
+        <ul className="list-none hidden 
+        sm:flex flex-row gap-10">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
-          })}
+          ))}
 
         </ul>
 

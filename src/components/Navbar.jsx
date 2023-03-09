6cd0d900847070b208248d330c22d0f3331e6399
carrycooldude <rawatkari554@gmail.com>
2023-03-09 22:58:14 +0530

    added menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { navLinks } from '../constants';
 import { logo , menu , close } from '../assets';
 
 const Navbar = () => {
-  const [active , setActive] = useState('');  
+  const [active , setActive] = useState('');
+  const [toggle , setToggle]= useState(false);  
   return (
     <nav className={
       `${styles.paddingX} w-full flex items-center 
@@ -39,6 +40,16 @@ const Navbar = () => {
           ))}
 
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex' } 
+          p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}></div>
+        </div>
 
       </div>
 

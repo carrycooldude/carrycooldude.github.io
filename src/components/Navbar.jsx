f3ff05afd6901f75adbdab24bc1d5ad572f15c00
carrycooldude <rawatkari554@gmail.com>
2023-03-10 12:26:44 +0530

    Changed the naming

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ import { logo , menu , close } from '../assets';
 
 const Navbar = () => {
   const [active , setActive] = useState('');
-  const [toggle , setToggle]= useState(false);  
+  const [toggle , setToggle] = useState(false); 
   return (
     <nav className={
       `${styles.paddingX} w-full flex items-center 
-    py-5 fixed top-0 z-20 bg-primary`}
+      py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
@@ -40,15 +40,14 @@ const Navbar = () => {
           ))}
 
         </ul>
-        <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img
-            src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
-            onClick={() => setToggle(!toggle)}
-          />
-          <div className={`${!toggle ? 'hidden' : 'flex' } 
-          p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}></div>
+
+        <div className="sm:hidden flex 
+          flex-1 justify-end items-center">
+          <img 
+            src={menu}
+            alt="menu" 
+            className="w-[28px] h-[28px] object-contain cursor-pointer" 
+            onClick={() => setToggle(!toggle)}/>
         </div>
 
       </div>

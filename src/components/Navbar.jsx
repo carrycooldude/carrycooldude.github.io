68cc44942a908bfdc874886fa9929cccbb78e943
carrycooldude <rawatkari554@gmail.com>
2023-03-10 19:47:27 +0530

    Done NavBar work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,10 @@ const Navbar = () => {
           }
         }>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Kartikey <span className="sm:block hidden">Rawat</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Kartikey &nbsp; 
+            <span className="sm:block hidden">Rawat</span>
+          </p>
         </Link>
         <ul className="list-none hidden 
         sm:flex flex-row gap-10">
@@ -52,6 +54,26 @@ const Navbar = () => {
               
             }}
           />
+          <div className={`${!toggle ? 'hidden' : 'flex' } p-6
+            black-gradient absolute top-20 right-0 mx-4 my-2
+            min-w-[140px] z-10 rounded-xl`}>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
           
           </div>
 

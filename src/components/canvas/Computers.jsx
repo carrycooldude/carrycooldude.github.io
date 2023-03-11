c897f0bfd7d84ff88660cba1f6a0863630974ad6
carrycooldude <rawatkari554@gmail.com>
2023-03-12 01:42:01 +0530

    Worked on Computer.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls , Preload , useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers  = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>

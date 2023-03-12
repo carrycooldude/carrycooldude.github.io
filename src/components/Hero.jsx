3562a13e12cbb4dc479c530a5a53320d7e0d5b40
carrycooldude <rawatkari554@gmail.com>
2023-03-12 13:52:45 +0530

    Rendered 3d model on browser

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,26 +5,26 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
     <section className="relative w-full 
-    h-screen mx-auto">
+      h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0
-      top-[120px] max-w-xl mx-auto flex flex-row 
-      items-start gap-5`}>
-        <div className="flex flex-col 
-        justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hey Folks, I am 
-          <span className="text-[#915eff]"> CarryCoolDude</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Founder at OpInCo Community | <br className="sm:block hidden"/>
-            GitHub Campus Expert 🚩
-          </p>
-        </div>
+        top-[120px] max-w-xl mx-auto flex flex-row 
+        items-start gap-5`}>
 
-      </div>
-      <ComputersCanvas/>
+          <div className="flex flex-col 
+          justify-center items-center mt-5">
+            <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>Hey Folks, I am 
+            <span className="text-[#915eff]"> CarryCoolDude</span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-50`}>
+              Founder at OpInCo Community | <br className="sm:block hidden"/>GitHub Campus Expert 🚩
+            </p>
+          </div>
+
+        </div>
+        <ComputersCanvas/>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,28 +1,64 @@
-import {Suspense , useEffect , useState} from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls , Preload , useGLTF } from '@react-three/drei';
-import CanvasLoader from '../Loader';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers  = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black"/>
-      <pointLight intensity={1}/>
-      <primitive 
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
         object={computer.scene}
-        scale={[0.75]}
-        position={[0,-3.25,-1.5]}
-        rotation={[-0.01,-0.2,-0.1]}/>
+        scale={isMobile ? 0.7 : 0.55}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.75, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
-}
+};
+
 const ComputersCanvas = () => {
-  return(
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
     <Canvas
       frameloop='demand'
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      dpr={[1, 2]}
+      camera={{ position: [20, 4, 6], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -31,7 +67,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />

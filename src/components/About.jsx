0763a6a6e6361a671e8a49cc257172b1b8820630
carrycooldude <rawatkari554@gmail.com>
2023-03-14 12:00:28 +0530

    Added Card

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,29 @@ import { services } from '../constants';
 import {fadeIn , textVariant} from '../utils/motion';
 const ServiceCard = ({index,title,icon}) => {
   return(
-    <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full" >
+      <motion.div
+       variants={fadeIn("right","spring",index*0.5,0.75)}
+       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div
+          options={{
+            max: 10,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
+           flex justify-evenly items-center flex-col">
+            <img src={icon} alt={title} className="w-20 h-20 object-contain" />
+            <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+
+        </div>
+
+      </motion.div>
+    </Tilt>
+
+
+
+      
   )
   
 };

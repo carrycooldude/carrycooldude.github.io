732097111c01a1c2d60320830e0f31f38229cae5
carrycooldude <rawatkari554@gmail.com>
2023-03-18 21:47:40 +0530

    Changes the size of logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             window.scrollTo(0,0);
           }
         }>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+          <img src={logo} alt="logo" className="w-20 h-20 object-contain"/>
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Kartikey &nbsp; 
             <span className="sm:block hidden">Rawat</span>
